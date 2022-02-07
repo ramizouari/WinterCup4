6df55f2aa5b4faccf5f1d5deef9b7abf807ed75f
Rami Zouari <zouari.rami@yahoo.com>
2022-02-07 20:43:00 +0100

    Updating D&C Solution

--- Groups of permutations/solution_greedy.cpp
+++ Groups of permutations/solution_greedy.cpp
@@ -2,47 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <deque>
-
-bool permutation_separable(const std::deque<int> &A)
-{
-	if (A.size() == 0)
-		return true;
-	int n = A.size();
-	auto u = std::max_element(A.begin(), A.end());
-	auto s = std::distance(A.begin(), u);
-	int a = std::max<int>(s - *u+1, 0),b=std::min<int>(s+*u,n);
-	if (b - a < *u)
-		return false;
-	std::vector<int> C(*u+1);
-	int distinct=0;
-	for (int i = a; i < a + *u; i++)
-	{
-		if (C[A[i]] == 0)
-			distinct++;
-		C[A[i]]++;
-	}
-	std::deque<int> A1,A2;
-	for (int k = 0; k < a; k++)
-		A1.push_back(A[k]);
-	for (int k = a+*u; k < n; k++)
-		A2.push_back(A[k]);
-	if (distinct == *u && permutation_separable(A1) && permutation_separable(A2))
-		return true;
-	for (int i = a+1; i <= b-*u; i++)
-	{
-		C[A[i - 1]]--;
-		if (C[A[i - 1]] == 0)
-			distinct--;
-		if (C[A2.front()] == 0)
-		{
-			distinct++;
-			A2.pop_front();
-		}
-		if (distinct == *u && permutation_separable(A1) && permutation_separable(A2))
-			return true;
-	}
-	return false;
-}
+#include <map>
 
 int main()
 {
@@ -51,5 +11,12 @@ int main()
 	std::deque<int> A(n);
 	for (auto& a : A)
 		std::cin >> a;
-	std::cout << (permutation_separable(A) ? "YES" : "NO") << '\n';
+	std::map<int, int> counter;
+	int size = 0;
+	for (auto a : A)
+	{
+		counter[a]++;
+		size++;
+
+	}
 }
